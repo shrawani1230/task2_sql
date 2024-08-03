303c0daf6a2a75b15a7e314d9554520e390c1281
shrawani1230 <sammeshram1988@gmail.com>
2024-08-04 03:33:45 +0530

    second commit for sql_task2

---
+++ sql_task2.sql
@@ -0,0 +1,12 @@
+INSERT INTO employees (employee_id,first_name,last_name,department_id) VALUES (128,'NAME125','yada',201)
+    INSERT INTO employees (employee_id,first_name,last_name,department_id) VALUES (129,'NAME126','karman',202)
+	INSERT INTO employees (employee_id,first_name,last_name,department_id) VALUES (130,'NAME127','tuli',203)
+INSERT INTO employees (employee_id,first_name,last_name,department_id) VALUES (131,'NAME128','jaiswal',204)
+INSERT INTO employees (employee_id,first_name,last_name,department_id) VALUES (132,'NAME129','roi',205)
+INSERT INTO employees (employee_id,first_name,last_name,department_id) VALUES (133,'NAME130','singh',206)   
+INSERT INTO employees (employee_id,first_name,last_name,department_id) VALUES (134,'NAME131','gill',207)
+INSERT INTO employees (employee_id,first_name,last_name,department_id) VALUES (135,'NAME132','rajput',208)
+INSERT INTO employees (employee_id,first_name,last_name,department_id) VALUES (136,'NAME133','barman',209)   
+	INSERT INTO employees (employee_id,first_name,last_name,department_id) VALUES (137,'NAME134','kanjilal',200)    
+
+select * from employees
